d246294be8921b2d8a384948c7712dc60208c531
AtharvaFTW <atharvavankhede@gmail.com>
2026-06-08 18:42:57 +0530

    added repition penality

--- src/inference.py
+++ src/inference.py
@@ -58,7 +58,11 @@ def generate_response(model, tokenizer, prompt:str, max_new_tokens:int = 200) ->
 
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
-    generated_ids = model.generate(**inputs, max_new_tokens = max_new_tokens,eos_token_id = tokenizer.eos_token_id, pad_token_id = tokenizer.eos_token_id)
+    generated_ids = model.generate(**inputs,
+                                    max_new_tokens = max_new_tokens,
+                                    eos_token_id = tokenizer.eos_token_id, 
+                                    pad_token_id = tokenizer.eos_token_id,
+                                    repetition_penality = 1.3)
 
     input_length = inputs["input_ids"].shape[1]
     response_ids = generated_ids[0][input_length:]
